bb562db1c648c51bcb2e0b3f7c2b6e4a44c94090
stonecompass <danielbross88@gmail.com>
2017-04-09 03:05:31 +0200

    Implemented config-file loading
    Former-commit-id: faf07520412ea736254ac8f3ea7855af64269c8a

--- src/main.cpp
+++ src/main.cpp
@@ -16,8 +16,45 @@ static void error_callback(int error, const char* description)
     fprintf(stderr, "Error: %s\n", description);
 }
 
+std::map<std::string,std::string> load_config(std::string filename)
+{
+    std::ifstream input(filename); //The input stream
+    std::map<std::string,std::string> ans; //A map of key-value pairs in the file
+    while(input) //Keep on going as long as the file stream is good
+    {
+        std::string key; //The key
+        std::string value; //The value
+        std::getline(input, key, ':'); //Read up to the : delimiter into key
+        std::getline(input, value, '\n'); //Read up to the newline into value
+        std::string::size_type pos1 = value.find_first_of("\""); //Find the first quote in the value
+        std::string::size_type pos2 = value.find_last_of("\""); //Find the last quote in the value
+        if(pos1 != std::string::npos && pos2 != std::string::npos && pos2 > pos1) //Check if the found positions are all valid
+        {
+            value = value.substr(pos1+1,pos2-pos1-1); //Take a substring of the part between the quotes
+            ans[key] = value; //Store the result in the map
+        }
+    }
+    input.close(); //Close the file stream
+    return ans; //And return the result
+}
+
 int main(void)
-{    
+{   
+    //load config file
+    std::string title;
+    std::string version;
+    int screenWidth;
+    int screenHeight;
+    bool fullscreen;
+
+    auto map = load_config("./assets/.config");
+
+    title = map["title"];
+    version = map["version"];
+    screenWidth = std::stoi(map["screen_width"]);
+    screenHeight = std::stoi(map["screen_height"]);
+    fullscreen = strcmp("true", map["fullscreen"].c_str()) == 0;
+
     GLFWwindow* window;
 
     glfwSetErrorCallback(error_callback);
@@ -28,7 +65,7 @@ int main(void)
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 
-    window = glfwCreateWindow(1080, 720, "Powder engine v0.0.1", NULL, NULL);
+    window = glfwCreateWindow(screenWidth, screenHeight, (title + std::string(" ") + version).c_str(), fullscreen ? glfwGetPrimaryMonitor() : NULL, NULL);
 
     //center window on screen
     const GLFWvidmode* mode = glfwGetVideoMode(glfwGetPrimaryMonitor());
