f68d0697268614af15ce4801a246d26f0574cd51
Niels Bross <nielsbross@gmail.com>
2017-04-16 00:27:10 +0200

    removed unused function warning
    Former-commit-id: 2c3e946d7cae255d2777290d0111ebef06d14c51

--- src/sound.cpp
+++ src/sound.cpp
@@ -3,11 +3,11 @@ static void PlaySound(loaded_sound *LoadedSound)
 {
     if (LoadedSound)
     {
-        alSourcef(LoadedSound->Source, AL_PITCH, 1);
-        alSourcef(LoadedSound->Source, AL_GAIN, 1);
-        alSource3f(LoadedSound->Source, AL_POSITION, 0, 0, 0);
-        alSource3f(LoadedSound->Source, AL_VELOCITY, 0, 0, 0);
-        alSourcei(LoadedSound->Source, AL_LOOPING, AL_FALSE);
+        alSourcef(LoadedSound->Source, AL_PITCH, LoadedSound->SoundInfo.Pitch);
+        alSourcef(LoadedSound->Source, AL_GAIN, LoadedSound->SoundInfo.Gain);
+        alSource3f(LoadedSound->Source, AL_POSITION, LoadedSound->SoundInfo.Position[0], LoadedSound->SoundInfo.Position[1], LoadedSound->SoundInfo.Position[2]);
+        alSource3f(LoadedSound->Source, AL_VELOCITY, LoadedSound->SoundInfo.Velocity[0], LoadedSound->SoundInfo.Velocity[1], LoadedSound->SoundInfo.Velocity[2]);
+        alSourcei(LoadedSound->Source, AL_LOOPING, LoadedSound->SoundInfo.Loop);
         alSourcePlay(LoadedSound->Source);
         alGetSourcei(LoadedSound->Source, AL_SOURCE_STATE, &LoadedSound->SourceState);
     }
@@ -22,5 +22,21 @@ static void PlaySoundOnce(loaded_sound *LoadedSound)
         {
             PlaySound(LoadedSound);
         }
+    }
+}
+
+static void PauseSound(loaded_sound *LoadedSound)
+{
+    if (LoadedSound)
+    {
+        alSourcePause(LoadedSound->Source);
+    }
+}
+
+static void StopSound(loaded_sound *LoadedSound)
+{
+    if (LoadedSound)
+    {
+        alSourceStop(LoadedSound->Source);
     }
 }
