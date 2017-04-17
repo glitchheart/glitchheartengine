8fac175040ad2fe7f3811c69fa38f5a835f168f4
stonecompass <danielbross88@gmail.com>
2017-04-17 16:41:29 +0200

    build.bat crap
    Former-commit-id: ddaddbc638dd4607410385f11df89f41437cdcbc

---
+++ libs/glfw/build.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+
+OPTIONS='-DCMAKE_BUILD_TYPE=Release -DGLFW_BUILD_TESTS=NO -DGLFW_BUILD_EXAMPLES=NO -DGLFW_BUILD_DOCS=NO'
+GLFWDIR='../../../glfw'
+
+mkdir -p glfw-bin.WIN32/lib-mingw
+mkdir -p glfw-bin.WIN32/lib-mingw-w64
+mkdir -p glfw-bin.WIN64/lib-mingw-w64
+
+# MinGW 32-bit
+
+mkdir -p build/mingw-x86
+pushd    build/mingw-x86
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$GLFWDIR/CMake/i686-pc-mingw32.cmake $OPTIONS -DBUILD_SHARED_LIBS=NO $GLFWDIR
+make
+cmake $OPTIONS -DBUILD_SHARED_LIBS=YES $GLFWDIR
+make
+
+popd
+
+cp build/mingw-x86/src/libglfw3.a    glfw-bin.WIN32/lib-mingw
+cp build/mingw-x86/src/libglfw3dll.a glfw-bin.WIN32/lib-mingw
+cp build/mingw-x86/src/glfw3.dll     glfw-bin.WIN32/lib-mingw
+
+# MinGW-w64 32-bit
+
+mkdir -p build/mingw-w64-x86
+pushd    build/mingw-w64-x86
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$GLFWDIR/CMake/i686-w64-mingw32.cmake $OPTIONS -DBUILD_SHARED_LIBS=NO $GLFWDIR
+make
+cmake $OPTIONS -DBUILD_SHARED_LIBS=YES $GLFWDIR
+make
+
+popd
+
+cp build/mingw-w64-x86/src/libglfw3.a    glfw-bin.WIN32/lib-mingw-w64
+cp build/mingw-w64-x86/src/libglfw3dll.a glfw-bin.WIN32/lib-mingw-w64
+cp build/mingw-w64-x86/src/glfw3.dll     glfw-bin.WIN32/lib-mingw-w64
+
+# MinGW-w64 64-bit
+
+mkdir -p build/mingw-w64-x64
+pushd    build/mingw-w64-x64
+
+cmake -DCMAKE_TOOLCHAIN_FILE=$GLFWDIR/CMake/x86_64-w64-mingw32.cmake $OPTIONS -DBUILD_SHARED_LIBS=NO $GLFWDIR
+make
+cmake $OPTIONS -DBUILD_SHARED_LIBS=YES $GLFWDIR
+make
+
+popd
+
+cp build/mingw-w64-x64/src/libglfw3.a    glfw-bin.WIN64/lib-mingw-w64
+cp build/mingw-w64-x64/src/libglfw3dll.a glfw-bin.WIN64/lib-mingw-w64
+cp build/mingw-w64-x64/src/glfw3.dll     glfw-bin.WIN64/lib-mingw-w64
+
+# Visual C++
+
+cmd /c build.bat
+
