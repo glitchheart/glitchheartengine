cab30ade1bf36a19bb9234cac4299f578301c219
Niels Bross <nielsbross@gmail.com>
2017-04-17 19:36:14 +0200

    can do live code reload again
    Former-commit-id: 079cfe2591deaa78044478878b7b00113f9accf9

--- build.bat
+++ build.bat
@@ -5,8 +5,8 @@ set WERROR=
 
 REM OLD set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi %WERROR% -W4  -wd4201 -wd4100 -wd4189 -wd4530 -wd4577 -wd4996 -FC -Z7 /I..\libs /I..\libs\glfw\include /I..\libs\openal\include /I..\libs\freetype\include
 
-set CommonCompilerFlags=/MT -nologo -Od -Oi -W4 -Gm- -EHsc -wd4201 -wd4100 -wd4189 -wd4530 -wd4577 -wd4996 -wd4456 -FC -Z7 /I..\libs /I..\libs\glfw\include /I..\libs\openal\include /I..\libs\freetype\include /I..\libs\glad\include 
-set CommonLinkerFlags= kernel32.lib user32.lib gdi32.lib winmm.lib opengl32.lib shell32.lib ..\libs\openal\libs\Win64\OpenAL32.lib ..\libs\glfw\lib-vc2015\glfw3.lib ..\libs\freetype\lib\freetype271d.lib glad.obj
+set CommonCompilerFlags=/MD -nologo -Od -Oi -W4 -Gm- -EHsc -wd4201 -wd4100 -wd4189 -wd4530 -wd4577 -wd4996 -wd4456 -FC -Z7 /I..\libs /I..\libs\glfw\include /I..\libs\openal\include /I..\libs\freetype\include /I..\libs\glad\include 
+set CommonLinkerFlags= kernel32.lib user32.lib gdi32.lib winmm.lib opengl32.lib shell32.lib ..\libs\openal\libs\Win64\OpenAL32.lib ..\libs\glfw\lib-vc2015\glfw3.lib ..\libs\freetype\lib\freetype271d.lib 
 set ExtraLinkerFlags=/NODEFAULTLIB:"LIBCMT" -incremental:no -opt:ref /ignore:4099
 
 REM TODO - can we just build both with one exe?
@@ -18,7 +18,7 @@ REM 64-bit build
 del *.pdb > NUL 2> NUL
 
 echo Compilation started on: %time%
-cl %CommonCompilerFlags% ..\src\game.cpp -Fmgame.map  %CommonLinkerFlags% -LD /DLL /link %ExtraLinkerFlags% -PDB:game%random%.pdb -EXPORT:Update 
-cl %CommonCompilerFlags% ..\src\main.cpp -Fmmain.map  %CommonLinkerFlags% /link %ExtraLinkerFlags%
+cl %CommonCompilerFlags% ..\src\game.cpp -Fmgame.map -LD /DLL /link %CommonLinkerFlags% %ExtraLinkerFlags% -PDB:game%random%.pdb -EXPORT:Update  
+cl %CommonCompilerFlags% ..\src\main.cpp -Fmmain.map /link %ExtraLinkerFlags% %CommonLinkerFlags% glad.obj
 echo Compilation finished on: %time%
 popd
