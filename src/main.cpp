eb3f5081b854b284a5e4307b7079552ebf27ca14
stonecompass <danielbross88@gmail.com>
2017-05-13 20:44:13 +0200

    Fixed some collision bugs, still gets stuck sometimes

--- src/main.cpp
+++ src/main.cpp
@@ -166,6 +166,7 @@ void SpawnMillionBarrels(game_state* GameState)
             Barrel.Velocity = glm::vec2(0,0);
             Barrel.Center = glm::vec2(0.5, 0.5);
             Barrel.IsStatic = true;
+            Barrel.IsKinematic = false;
             Barrel.Pickup = true;
             
             collision_AABB CollisionAABB;
@@ -282,11 +283,27 @@ int main(void)
     collision_AABB CollisionAABB;
     CollisionAABB.Center = glm::vec2(Player.Position.x + Player.Center.x * Player.Scale.x,
                                      Player.Position.y + Player.Center.y * Player.Scale.y);
-    CollisionAABB.Extents = glm::vec2(0.3f, 0.1f);
+    CollisionAABB.Extents = glm::vec2(0.3f, 0.2f);
     Player.CollisionAABB = CollisionAABB;
     Player.Center = glm::vec2(0.53f, 0.975f);
     Player.EntityIndex = GameState.EntityCount;
     GameState.Entities[GameState.EntityCount++] = Player;
+    
+    entity PlayerWeapon;
+    PlayerWeapon.RenderEntity.Rendered = false;
+    PlayerWeapon.Name = "Player weapon";
+    PlayerWeapon.Type = Entity_PlayerWeapon;
+    
+    collision_AABB CollisionAABB3;
+    CollisionAABB3.Center = glm::vec2(0.5, 0.5);
+    CollisionAABB3.Extents = glm::vec2(0.5f,0.5f);
+    CollisionAABB3.IsTrigger = true;
+    PlayerWeapon.CollisionAABB = CollisionAABB3;
+    PlayerWeapon.Rotation = glm::vec3(0, 0, 0);
+    PlayerWeapon.Scale = glm::vec3(1, 1, 0);
+    
+    PlayerWeapon.EntityIndex = GameState.EntityCount;
+    GameState.Entities[GameState.EntityCount++] = PlayerWeapon;
     
     entity Crosshair = {};
     Crosshair.Name = "Crosshair";
