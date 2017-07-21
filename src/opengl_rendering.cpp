76a82e632cc9e1339cab3eb376a75c862c340366
stonecompass <danielbross88@gmail.com>
2017-07-22 01:08:53 +0200

    Fixed render sorting problem.
    Former-commit-id: abce4400797372a30f8ba1fc2fe0dab7059cf1f2

--- src/opengl_rendering.cpp
+++ src/opengl_rendering.cpp
@@ -1929,7 +1929,7 @@ int CompareFunction(const void* a, const void* b)
     
     if(APtr.Background || BoxA.Center.y - BoxA.Extents.y > BoxB.Center.y - BoxB.Extents.y)
         return -1;
-    if(BoxA.Center.y  - BoxA.Extents.y < BoxB.Center.y - BoxB.Extents.y || BPtr.Background)
+    if(BPtr.Background || BoxA.Center.y  - BoxA.Extents.y < BoxB.Center.y - BoxB.Extents.y)
         return 1;
     return 0;
 }
