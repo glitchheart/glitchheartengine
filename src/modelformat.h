fecdd24b97c3e5479fbd506973466e117c533f16
Niels Bross <nielsbross@gmail.com>
2017-08-14 22:03:56 +0200

    loading basic model header

--- src/modelformat.h
+++ src/modelformat.h
@@ -1,24 +1,24 @@
-
+#ifndef MODEL_FORMAT_H
+#define MODEL_FORMAT_H
 
 struct model_header
 {
     char Format[4]; // M O D L ?
-    short NumMeshes;
 };
 
 struct faces_chunk
 {
     char FacesChunkID[4]; // F C E S
     long FacesChunkSize;
-    void* FacesData; // Index buffer?
+    unsigned char* FacesData; // Index buffer?
 };
 
 struct vertex_buffer_chunk
 {
     char ChunkID[4]; // V B O _
-    char Format[4]; // V U N _ / V N _ _ / V _ _ _
+    char Format[4]; // V U N C / V U N _ / V N _ _ / V _ _ _
     long ChunkSize;
-    void* Data; // This is the VBO data in the format specified
+    unsigned char* Data; // This is the VBO data in the format specified
 };
 
-
+#endif
