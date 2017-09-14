754b9bcdb9d315a5152e5ebe6ea02ddeda6666ea
Niels Bross <nielsbross@gmail.com>
2017-09-14 20:54:49 +0200

    more failing sat
    Former-commit-id: ef8b2a39e0debb14795a1d55f71512024b1128d3

--- src/collision.cpp
+++ src/collision.cpp
@@ -261,56 +261,56 @@ int TestOBBOBB(collision_volume &a, collision_volume &b)
     // their cross product is (near) null (see text for details)
     for (int i = 0; i < 3; i++)
         for (int j = 0; j < 3; j++)
-        AbsR[i][j] = Abs(R[i][j]) + EPSILON;
+        AbsR[i][j] = math::Absolute(R[i][j]) + EPSILON;
     // Test axes L = A0, L = A1, L = A2
     for (int i = 0; i < 3; i++) {
         ra = a.e[i];
         rb = b.e[0] * AbsR[i][0] + b.e[1] * AbsR[i][1] + b.e[2] * AbsR[i][2];
-        if (Abs(t[i]) > ra + rb) return 0;
+        if (math::Absolute(t[i]) > ra + rb) return 0;
     }
     // Test axes L = B0, L = B1, L = B2
     for (int i = 0; i < 3; i++) {
         ra = a.e[0] * AbsR[0][i] + a.e[1] * AbsR[1][i] + a.e[2] * AbsR[2][i];
         rb = b.e[i];
-        if (Abs(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return 0;
+        if (math::Absolute(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return 0;
     }
     // Test axis L = A0 x B0
     ra = a.e[1] * AbsR[2][0] + a.e[2] * AbsR[1][0];
     rb = b.e[1] * AbsR[0][2] + b.e[2] * AbsR[0][1];
-    if (Abs(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return 0;
+    if (math::Absolute(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return 0;
     // Test axis L = A0 x B1
     ra = a.e[1] * AbsR[2][1] + a.e[2] * AbsR[1][1];
     rb = b.e[0] * AbsR[0][2] + b.e[2] * AbsR[0][0];
-    if (Abs(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return 0;
+    if (math::Absolute(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return 0;
     // Test axis L = A0 x B2
     ra = a.e[1] * AbsR[2][2] + a.e[2] * AbsR[1][2];
     rb = b.e[0] * AbsR[0][1] + b.e[1] * AbsR[0][0];
-    if (Abs(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return 0;
+    if (math::Absolute(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return 0;
     // Test axis L = A1 x B0
     ra = a.e[0] * AbsR[2][0] + a.e[2] * AbsR[0][0];
     rb = b.e[1] * AbsR[1][2] + b.e[2] * AbsR[1][1];
     
-    if (Abs(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return 0;
+    if (math::Absolute(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return 0;
     // Test axis L = A1 x B1
     ra = a.e[0] * AbsR[2][1] + a.e[2] * AbsR[0][1];
     rb = b.e[0] * AbsR[1][2] + b.e[2] * AbsR[1][0];
-    if (Abs(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return 0;
+    if (math::Absolute(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return 0;
     // Test axis L = A1 x B2
     ra = a.e[0] * AbsR[2][2] + a.e[2] * AbsR[0][2];
     rb = b.e[0] * AbsR[1][1] + b.e[1] * AbsR[1][0];
-    if (Abs(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return 0;
+    if (math::Absolute(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return 0;
     // Test axis L = A2 x B0
     ra = a.e[0] * AbsR[1][0] + a.e[1] * AbsR[0][0];
     rb = b.e[1] * AbsR[2][2] + b.e[2] * AbsR[2][1];
-    if (Abs(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return 0;
+    if (math::Absolute(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return 0;
     // Test axis L = A2 x B1
     ra = a.e[0] * AbsR[1][1] + a.e[1] * AbsR[0][1];
     rb = b.e[0] * AbsR[2][2] + b.e[2] * AbsR[2][0];
-    if (Abs(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return 0;
+    if (math::Absolute(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return 0;
     // Test axis L = A2 x B2
     ra = a.e[0] * AbsR[1][2] + a.e[1] * AbsR[0][2];
     rb = b.e[0] * AbsR[2][1] + b.e[1] * AbsR[2][0];
-    if (Abs(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return 0;
+    if (math::Absolute(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return 0;
     // Since no separating axis is found, the OBBs must be intersecting
     return 1;
 }
