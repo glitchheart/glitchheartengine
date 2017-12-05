552e0b86d4b12c4bd6b9a0ef426f5fc23d93cb62
Niels Bross <nielsbross@gmail.com>
2017-12-05 20:15:46 +0100

    activated intrinsics and assert now creates a breakpoint rather than an abort
    Former-commit-id: 7915246d50323a1348f61033ef11853714589134

--- actual_build.bat
+++ actual_build.bat
@@ -10,7 +10,7 @@ WHERE cl
 IF %ERRORLEVEL% NEQ 0 call %VCVARSALL% x64
 
 echo %cd%
-set CommonCompilerFlags=/MD -nologo -Od -Oi- -Wall -Gm- -EHsc -FC -Z7 %PRP% %WIGNORED% %DEBUG% %GLM% /I..\libs /I..\libs\glfw\include /I..\libs\freetype\include /I..\libs\glad\include /I ..\libs\fmod\include
+set CommonCompilerFlags=/MD -nologo -Od -Oi -Wall -Gm- -EHsc -FC -Z7 %PRP% %WIGNORED% %DEBUG% %GLM% /I..\libs /I..\libs\glfw\include /I..\libs\freetype\include /I..\libs\glad\include /I ..\libs\fmod\include
 set CommonLinkerFlags= kernel32.lib user32.lib gdi32.lib winmm.lib opengl32.lib shell32.lib  ..\libs\glfw\lib-vc2015\glfw3.lib ..\libs\glad\glad.obj ..\libs\freetype\lib\freetype271d.lib ..\libs\fmod\lib\fmod64_vc.lib
 set ExtraLinkerFlags=/NODEFAULTLIB:"LIBCMT" -incremental:no -opt:ref /ignore:4099
 
