b9ae8619e678758a29dcea26eb5852d742e610ee
Daniel Bross <daniel@glitchheart.com>
2018-03-07 23:52:10 +0100

    Started implementing MoltenVK
    Former-commit-id: 50b9527b9af48b6e309de6401c4fe12a1536be73

--- osx_actual_build.sh
+++ osx_actual_build.sh
@@ -3,10 +3,10 @@
 WIGNORE="-Wno-nested-anon-types -Wno-variadic-macros -Wno-old-style-cast -Wno-sign-conversion -Wno-cast-align -Wno-gnu-anonymous-struct -Wno-c++98-compat -Wno-padded -Wdocumentation -Wdocumentation-unknown-command -Wno-double-promotion -Wno-c++98-compat-pedantic -Wno-missing-prototypes -Wno-float-equal -Wno-unused-parameter  -Wno-gnu-zero-variadic-macro-arguments -Wno-writable-strings -Wno-global-constructors -Wno-switch-enum -Wno-char-subscripts -Wno-unused-function"
 
 CommonCompilerFlags="-isystem ../libs -isystem ../libs/glfw/include  -isystem ../libs/FreeType/include -isystem ../libs/glad/include -I../libs/glad/src/glad.c -isystem ../libs/fmod/include -g"
-CommonLinkerFlags=" -L../libs/glad/osx/ -L/usr/local/lib -L../libs/glfw/osx/ -L../libs/fmod/lib/osx/ -ldl -lfreetype -lm -lpng -lz -lfmod -lglfw3 -lglad -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo ../libs/glad/osx/glad.o -rpath @executable_path -o main"
+CommonLinkerFlags=" -L../libs/glad/osx/ -L/usr/local/lib -L../libs/glfw/osx/ -L../libs/fmod/lib/osx/ -ldl -lfreetype -lm -lpng -lz -lfmod -lglfw3 -lglad -framework Cocoa -framework OpenGL -framework IOKit -framework CoreVideo -F$VULKAN_SDK/Frameworks -framework Vulkan ../libs/glad/osx/glad.o -rpath @executable_path -o main"
 
 pushd build
 
-clang --std=c++14 -Weverything $WIGNORE -DGLITCH_DEBUG=1 $CommonCompilerFlags -O0 ../src/main.cpp $CommonLinkerFlags
+clang --std=c++14 -Weverything $WIGNORE -DGLITCH_DEBUG=1 $CommonCompilerFlags -I$VULKAN_SDK/include -O0 ../src/main.cpp $CommonLinkerFlags
 
 popd
