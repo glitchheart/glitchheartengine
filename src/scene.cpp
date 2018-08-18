fe18221ea758441781a46862a9151f847f4b8f98
stonecompass <danielbross88@gmail.com>
2018-08-18 21:30:13 +0200

    Entity unregister-function added. All arrays are packed after unregistering an entity.
    Former-commit-id: ef365859abcfa2aecf360ced084abfe69aa3135b

--- src/scene.cpp
+++ src/scene.cpp
@@ -1,3 +1,5 @@
+#define ENTITY_IS_VALID(handle) handle.handle > 0
+
 namespace scene
 {
     // Creates and returns a new "Scene". The "inital_entity_array_size" specifies the max entity count of the "Scene".
@@ -5,18 +7,21 @@ namespace scene
     static Scene create_scene(Renderer renderer, i32 initial_entity_array_size = 1024)
     {
         Scene scene = {};
+        scene.max_entity_count = initial_entity_array_size;
         scene.entity_count = 0;
+        scene.current_internal_handle = 0;
         scene.transform_component_count = 0;
         scene.render_component_count = 0;
         scene.entities = (Entity*)malloc(sizeof(EntityHandle) * initial_entity_array_size);
+        scene._internal_handles = (i32*)malloc(sizeof(i32) * initial_entity_array_size);
         scene.active_entities = (b32*)malloc(sizeof(b32) * initial_entity_array_size);
         scene.transform_components = (TransformComponent*)malloc(sizeof(TransformComponent) * initial_entity_array_size);
         scene.render_components = (RenderComponent*)malloc(sizeof(RenderComponent) * initial_entity_array_size);
         scene.material_instances = (RenderMaterial*)malloc(sizeof(RenderMaterial) * initial_entity_array_size);
-        scene.byte_size = (sizeof(EntityHandle) + sizeof(TransformComponent) + sizeof(RenderComponent)) * initial_entity_array_size;
         
         for(i32 index = 0; index < initial_entity_array_size; index++)
         {
+            scene._internal_handles[index] = -1;
             scene.active_entities[index] = true;
             scene.transform_components[index].position = math::Vec3(0, 0, 0);
             scene.transform_components[index].scale = math::Vec3(1, 1, 1);
@@ -36,18 +41,61 @@ namespace scene
             scene.render_component_count = 0;
             
             free(scene.entities);
+            free(scene._internal_handles);
+            free(scene.active_entities);
             free(scene.transform_components);
             free(scene.render_components);
-            free(scene.active_entities);
             free(scene.material_instances);
+            
+            for(i32 index = 0; index < initial_entity_array_size; index++)
+            {
+                scene._internal_handles[index] = -1;
+                scene.active_entities[index] = true;
+                scene.transform_components[index].position = math::Vec3(0, 0, 0);
+                scene.transform_components[index].scale = math::Vec3(1, 1, 1);
+                scene.transform_components[index].rotation = math::Vec3(0, 0, 0);
+            }
         }
+    }
+    
+    i32 _unused_entity_handle(Scene &scene)
+    {
+        for(i32 index = scene.current_internal_handle; index < scene.max_entity_count; index++)
+        {
+            i32 i = scene._internal_handles[index];
+            if(i == -1)
+            {
+                scene.current_internal_handle = index;
+                return(index);
+            }
+        }
+        
+        // start from 0 afterwards
+        for(i32 index = 0; index < scene.current_internal_handle; index++)
+        {
+            i32 i = scene._internal_handles[index];
+            if(i == -1)
+            {
+                scene.current_internal_handle = index;
+                return(index);
+            }
+        }
+        
+        debug("All entities are in use.");
+        assert(false);
+        
+        return -1;
     }
     
     // Returns a new valid "EntityHandle". "comp_flags" Specifies the components that the entity should contain.
     static EntityHandle register_entity(u64 comp_flags, Scene &scene)
     {
-        EntityHandle handle = { scene.entity_count++ };
-        Entity &entity = scene.entities[handle.handle];
+        i32 new_handle = _unused_entity_handle(scene) + 1;
+        
+        EntityHandle handle = { new_handle };
+        scene._internal_handles[new_handle - 1] = scene.entity_count++;
+        
+        Entity &entity = scene.entities[scene._internal_handles[new_handle - 1]];
         entity.comp_flags = comp_flags;
         
         if(comp_flags & COMP_TRANSFORM)
@@ -67,15 +115,114 @@ namespace scene
         return(handle);
     }
     
+    i32 _pack_transform_components(Entity &entity, Scene &scene)
+    {
+        if(entity.comp_flags & COMP_TRANSFORM)
+        {
+            for(i32 index = entity.transform_handle.handle; index < scene.transform_component_count - 1; index++)
+            {
+                scene.transform_components[index] = scene.transform_components[index + 1];
+            }
+            scene.transform_component_count--;
+            return entity.transform_handle.handle;
+        }
+        return -1;
+    }
+    
+    i32 _pack_render_components(Entity &entity, Scene &scene)
+    {
+        if(entity.comp_flags & COMP_RENDER)
+        {
+            for(i32 index = entity.render_handle.handle; index < scene.render_component_count - 1; index++)
+            {
+                scene.render_components[index] = scene.render_components[index + 1];
+                scene.material_instances[index] = scene.material_instances[index + 1];
+                scene.render_components[index].material_handle.handle--;
+            }
+            scene.render_component_count--;
+            scene.material_count--;
+            return entity.render_handle.handle;
+        }
+        return -1;
+    }
+    
+    static void unregister_entity(EntityHandle &handle, Scene &scene)
+    {
+        if(handle.handle == 0)
+            return;
+        
+        i32 removed_handle = handle.handle;
+        
+        // Invalidate handle
+        handle.handle = 0;
+        
+        // If only one entity was present, we should just zero everything...
+        if(scene.entity_count == 1)
+        {
+            scene.entity_count = 0;
+            scene.transform_component_count = 0;
+            scene.render_component_count = 0;
+            scene.current_internal_handle = 0;
+        }
+        else
+        {
+            // Get the handle into the real entity array
+            i32 real_handle = scene._internal_handles[removed_handle - 1];
+            Entity &entity = scene.entities[real_handle];
+            
+            // Pack the components in scene by removing the unregistered entities components and moving the rest to pack the arrays. If the returned handles are -1 the entity didn't have that component set.
+            i32 transform_handle = _pack_transform_components(entity, scene);
+            i32 render_handle = _pack_render_components(entity, scene);
+            
+            // Remember to reset the internal handle to -1 to indicate an unused index
+            scene._internal_handles[removed_handle - 1] = -1;
+            
+            // Run through all existing entities and pack the array to remove the unregistered one. All component handles affected by the removal are decremented.
+            for(i32 index = real_handle; index < scene.entity_count - 1; index++)
+            {
+                scene.entities[index] = scene.entities[index + 1];
+                
+                if(scene.entities[index].transform_handle.handle > transform_handle)
+                {
+                    scene.entities[index].transform_handle.handle--;
+                }
+                
+                if(scene.entities[index].render_handle.handle > render_handle)
+                {
+                    scene.entities[index].render_handle.handle--;
+                }
+                
+                scene.active_entities[index] = scene.active_entities[index + 1];
+            }
+            
+            scene.entity_count--;
+            
+            // We also have to update the internal handles if they were placed after the unregistered entity
+            for(i32 internal_index = 0; internal_index < scene.max_entity_count; internal_index++)
+            {
+                i32 current_handle = scene._internal_handles[internal_index];
+                if(current_handle > -1 && current_handle > real_handle)
+                {
+                    scene._internal_handles[internal_index] = current_handle - 1;
+                }
+            }
+        }
+    }
+    
     static void set_active(EntityHandle handle, b32 active, Scene &scene)
     {
-        scene.active_entities[handle.handle] = active;
+        scene.active_entities[handle.handle - 1] = active;
     }
     
     // Returns a direct pointer to the TransformComponent of the specified entity
     static TransformComponent* get_transform_comp(EntityHandle handle, Scene &scene)
     {
-        Entity entity = scene.entities[handle.handle];
+        i32 internal_handle = scene._internal_handles[handle.handle - 1];
+        
+        if(internal_handle == -1)
+            return nullptr;
+        
+        Entity entity = scene.entities[internal_handle];
         
         assert(entity.comp_flags & COMP_TRANSFORM);
         
@@ -87,7 +234,12 @@ namespace scene
     // Returns a direct pointer to the RenderComponent of the specified entity
     static RenderComponent* get_render_comp(EntityHandle handle, Scene &scene)
     {
-        Entity entity = scene.entities[handle.handle];
+        i32 internal_handle = scene._internal_handles[handle.handle - 1];
+        
+        if(internal_handle == -1)
+            return nullptr;
+        
+        Entity entity = scene.entities[internal_handle];
         
         assert(entity.comp_flags & COMP_RENDER);
         
@@ -104,7 +256,10 @@ namespace scene
     
     static RenderMaterial & get_material(EntityHandle handle, Scene &scene)
     {
-        Entity &entity = scene.entities[handle.handle];
+        i32 internal_handle = scene._internal_handles[handle.handle - 1];
+        assert(internal_handle != -1);
+        
+        Entity &entity = scene.entities[internal_handle];
         return scene.material_instances[entity.render_handle.handle];
     }
 }
