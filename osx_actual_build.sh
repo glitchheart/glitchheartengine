eb31bd0a352d574b7b9e563364204110373cee7e
stonecompass <daniel@glitchheart.com>
2018-09-08 14:56:14 +0200

    Fixed bug when optimizations were on.
    Former-commit-id: a353aa6ce64f0ab9e2f66045a4948f7229688258

--- osx_actual_build.sh
+++ osx_actual_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DEBUG=-DDEBUG=0
+OPT=-O3
 
 WIGNORE="-Wno-nested-anon-types -Wno-variadic-macros -Wno-old-style-cast -Wno-sign-conversion -Wno-cast-align -Wno-gnu-anonymous-struct -Wno-c++98-compat -Wno-padded -Wdocumentation -Wdocumentation-unknown-command -Wno-double-promotion -Wno-c++98-compat-pedantic -Wno-missing-prototypes -Wno-float-equal -Wno-unused-parameter  -Wno-gnu-zero-variadic-macro-arguments -Wno-writable-strings -Wno-global-constructors -Wno-switch-enum -Wno-char-subscripts -Wno-unused-function"
 
@@ -9,6 +10,6 @@ CommonLinkerFlags="../libs/glad/osx/glad.o -L/usr/local/lib -L../libs/glfw/osx/ 
 
 pushd build
 
-clang --std=c++17 -Weverything $WIGNORE $DEBUG $CommonCompilerFlags -g -O0 ../src/main.cpp $CommonLinkerFlags
+clang --std=c++17 -Weverything $WIGNORE $DEBUG $CommonCompilerFlags -g $OPT ../src/main.cpp $CommonLinkerFlags
 
 popd
