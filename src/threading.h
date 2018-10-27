265f9851d00a50ebf077d54b7a3f996e518b2096
stonecompass <danielbross88@gmail.com>
2018-10-27 15:05:04 +0200

    Started threading API.
    Former-commit-id: 7ab275c33b552b6f91a6884b2d255504768e0590

---
+++ src/threading.h
@@ -0,0 +1,21 @@
+#ifndef THREADING_H
+#define THREADING_H
+
+struct ThreadHandle
+{
+	i32 handle;
+};
+
+enum class ThreadCreationFlags
+{
+	// @Incomplete
+};
+
+struct ThreadData
+{};
+
+struct ThreadState
+{};
+
+#endif
+
