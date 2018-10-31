6a4c4d44b617b25cd290077f44044bcffd0c389d
stonecompass <danielbross88@gmail.com>
2018-10-31 21:37:03 +0100

    Added libcurl for Windows.
    Former-commit-id: 2ff41edbcef7e2e15caa43534759a3f1f1dda03d

--- src/win32_threading.cpp
+++ src/win32_threading.cpp
@@ -3,22 +3,23 @@
 
 // Define platform-specific functions
 #define SemaphoreHandle HANDLE
-#define THREAD_PROC(name) static DWORD name(*void)
+#define THREAD_PROC(name) static DWORD name(void* parameters)
 #define WRITE_BARRIER _WriteBarrier(); _mm_sfence()
+typedef DWORD (*ThreadProc)(void* parameters);
 
-static inline u32 interlocked_compare_exchang(u32 *ptr, u32 next, u32 original)
+static inline u32 interlocked_compare_exchange(u32 volatile *ptr, u32 next, u32 original)
 {
-    InterlockedCompareExchange(ptr, next, original);
+    return (u32)InterlockedCompareExchange(ptr, next, original);
 }
 
-static inline void interlocked_increment(u32 *ptr)
+static inline void interlocked_increment(u32 volatile *ptr)
 {
     InterlockedIncrement((LONG volatile *)ptr);
 }
 
-static inline void release_semaphore(SemaphoreHandle semaphore_handle, i32 count)
+static inline void release_semaphore(SemaphoreHandle semaphore_handle)
 {
-    ReleaseSemaphore(queue->semaphore_handle, count, 0);
+    ReleaseSemaphore(semaphore_handle, 1, 0);
 }
 
 static inline void wait_for_semaphore(SemaphoreHandle semaphore_handle)
@@ -28,17 +29,15 @@ static inline void wait_for_semaphore(SemaphoreHandle semaphore_handle)
 
 static inline SemaphoreHandle create_semaphore(u32 initial_count, u32 thread_count)
 {
-    return CreateSemaphoreEx(0, initial_count, thread_count, 0, 0, SEMAPHORE_ALL_ACCESS);
+    return CreateSemaphoreEx(0, (LONG)initial_count, (LONG)thread_count, 0, 0, SEMAPHORE_ALL_ACCESS);
 }
 
 static inline void create_thread(ThreadProc thread_proc, void *parameter)
 {
     DWORD thread_id;
-    HANDLE thread_handle = CreateThread(0, 1000000, thread_proc, thread_info, thread_id);
+    HANDLE thread_handle = CreateThread(0, 1000000, thread_proc, parameter, 0, &thread_id);
     CloseHandle(thread_handle);
 }
-
-#endif
 
 #include "threading.h"
 #include "threading.cpp"
