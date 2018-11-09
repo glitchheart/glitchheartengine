724e24c8041cc8f9b300d97ba9c6ec0b8a691d8b
stonecompass <danielbross88@gmail.com>
2018-11-09 22:41:02 +0100

    Merged develop into master.

--- actual_build.bat
+++ actual_build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set WERROR=
-set DEBUG=-DDEBUG=1
+set DEBUG=0
 set GLM=
 set PRP=
 set WIGNORED=-wd5039 -wd4201 -wd4505 -wd4100 -wd4996 -wd4456 -wd4582 -wd4587 -wd4820 -wd4061 -wd4710 -wd4191 -wd4623 -wd4625 -wd5026 -wd4668 -wd4312 -wd5045 -wd4577 -wd4711 -wd4514
@@ -15,11 +15,11 @@ WHERE cl
 IF %ERRORLEVEL% NEQ 0 call %VCVARSALL% x64
 
 echo %cd%
-set CommonCompilerFlags=/MD /arch:AVX -std:c++latest -fp:fast -fp:except- -nologo %OPT% -Oi -W4 -Gm- -GR- -EHa -FC -Z7 %PRP% %WIGNORED% %DEBUG%  %GLM% /I..\libs /I%AnalyticsSrcPath%\gameanalytics\ /I..\libs\glfw\include /I..\libs\libcurl\include  /I..\libs\glad\include /I ..\libs\fmod\include /I..\libs\vulkan\Include
+set CommonCompilerFlags=/MD /arch:AVX -std:c++latest -fp:fast -fp:except- -nologo %OPT% -Oi -W4 -Gm- -GR- -EHa -FC -Z7 %PRP% %WIGNORED% -DDEBUG=%DEBUG% %GLM% /I..\libs /I%AnalyticsSrcPath%\gameanalytics\ /I..\libs\glfw\include /I..\libs\libcurl\include  /I..\libs\glad\include /I ..\libs\fmod\include /I..\libs\vulkan\Include
 set CommonLinkerFlags= Comdlg32.lib Ole32.lib kernel32.lib user32.lib gdi32.lib winmm.lib opengl32.lib shell32.lib Ws2_32.lib Wldap32.lib Crypt32.lib Normaliz.lib Advapi32.lib ..\libs\libcurl\win\libcurl_a.lib ..\libs\glfw\lib-vc2015\glfw3.lib ..\libs\glad\glad.obj ..\libs\fmod\lib\fmod64_vc.lib %AnalyticsDeps%\curl\lib\win64\vc140_x64_release\libcurl.lib %AnalyticsDeps%\openssl\1.0.2h\libs\win64\vc140_x64_release\libeay32.lib %AnalyticsDeps%\openssl\1.0.2h\libs\win64\vc140_x64_release\ssleay32.lib %AnalyticsPath%\win64-vc140-static\Release\GameAnalytics.lib
 set ExtraLinkerFlags=/NODEFAULTLIB:"LIBCMT" -incremental:no -opt:ref /ignore:4099
 
-if defined DEBUG set ExtraLinkerFlags = %ExtraLinkerFlags% /subsystem:windows /entry:mainCRTStartup
+REM IF %DEBUG% EQU 0 set ExtraLinkerFlags = %ExtraLinkerFlags% /subsystem:windows /entry:mainCRTStartup
 
 IF NOT EXIST build mkdir build
 pushd build
