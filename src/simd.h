5e92f9bc9255c42477e3928af97742986662ecb9
stonecompass <daniel@glitchheart.com>
2018-11-23 16:31:48 +0100

    Fixed memory alignment problem.
    Former-commit-id: ec8d910589b940ca5974401be501962a69315be0

--- src/simd.h
+++ src/simd.h
@@ -2041,10 +2041,11 @@ inline void vec4_4x_to_float4(Vec4_4x v, float* f1, float* f2, float* f3, float*
 
 #define member_size(type, member) sizeof(((type *)0)->member)
 
-#define push_array_simd(arena, count, type) (type *)_push_size_simd(arena, (count) * sizeof(type), (member_size(type, e[0]) * 2))
-#define push_size_simd(arena, type) (type*)_push_size_simd(arena, sizeof(type), (member_size(type, e[0]) * 2))
+#define push_array_simd(arena, count, type) (type *)_push_size_simd(arena, (count) * sizeof(type), (member_size(type, e[0])))
+#define push_size_simd(arena, type) (type*)_push_size_simd(arena, sizeof(type), (member_size(type, e[0])))
 inline void* _push_size_simd(MemoryArena *arena, umm size_init, u32 alignment)
 {
+    alignment = math::multiple_of_number(alignment, 16);
     return push_size_(arena, size_init, default_with_alignment(alignment));
 }
 
