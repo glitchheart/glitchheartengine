e4b1004bce8e3de0d3579fecdaf8ce75c4ad8ac5
Niels Bross <nielsbross@gmail.com>
2019-04-17 21:04:03 +0200

    mipmap levels

--- src/opengl_rendering.cpp
+++ src/opengl_rendering.cpp
@@ -1145,7 +1145,10 @@ static void load_texture(Texture* texture, TextureFiltering filtering, TextureWr
     glGenerateMipmap(GL_TEXTURE_2D);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 4);
+
+    i32 mip_max_level = math::ceil(log2(MAX(width,height))) + 1;
+    
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, mip_max_level);
     //glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_LOD_BIAS, -1);
 }
 
