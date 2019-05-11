9f69dfb5f5e8937cba41ab310eb5276ea81b31f4
stonecompass <danielbross88@gmail.com>
2019-05-11 14:27:18 +0200

    Prepared for demo.
    Former-commit-id: 32b45c6c90228e614d2f89a2f318c505f87be2f7

--- src/shader_loader.cpp
+++ src/shader_loader.cpp
@@ -1176,13 +1176,13 @@ namespace rendering
 
 			sprintf(out_path, "../out/%s.shd", out_name);
 
-			FILE *shd = fopen(out_path, "w");
-			fwrite(shader.vert_shader, strlen(shader.vert_shader), 1, shd);
-			fwrite(shader.frag_shader, strlen(shader.frag_shader), 1, shd);
-			fclose(shd);
+            // FILE *shd = fopen(out_path, "w");
+			// fwrite(shader.vert_shader, strlen(shader.vert_shader), 1, shd);
+			// fwrite(shader.frag_shader, strlen(shader.frag_shader), 1, shd);
+			// fclose(shd);
 #endif
 
-			fclose(file);
+                    //fclose(file);
 			shader.loaded = shader.vert_shader && shader.frag_shader;
 
             if(!reload)
